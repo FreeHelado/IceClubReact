4b1b6ec4de5cb5fd4ce11a069fcaaa617cadcb8b
Alejandro Ferreira <alejandro@i2es.info>
2023-09-15 22:33:16 -0300

    Carrito y order funcionando y descontando stock

--- src/App.jsx
+++ src/App.jsx
@@ -11,55 +11,6 @@ import { CartProvider } from './context/CartContext';
 
 const App = () => {
 
-  // const createOrder = () => {
-  //   const order = {
-  //     buyer: { name:"Alitoh", phone: "099128034", email: "alejandro@i2es.info" },
-  //     items: products[0],
-  //     total: products[0].price
-  //   }
-
-  //   const orderColletion = collection(db, 'orders'),
-  //   addDoc(orderColletion, order).then(({id}) => console.log(id))
-  // }
-
-  //FIREBASE - LLAMAR A UN SOLO DOCUMENTO
-  // const productRef = doc(db, "products", "4dKZoGjw0GjLgIjOqyOC")
-
-  // const getProduct = () => {
-  //   getDoc(productRef).then((snapshot => {
-  //     if(snapshot.exists()) {
-  //       console.log({ id: snapshot.id, ...snapshot.data() })
-  //     }
-  //   }))
-  // }
-
-  // useEffect(() => {
-  //   getProduct();
-  // }, []);
-  //FIREBASE - LLAMAR A UN SOLO DOCUMENTO
-
-  ///FIREBASE - LLAMAR A TODOS LOS DOCUMENTOS
-  // const collectionRef = collection(db, "discos")
-  // const getCollection = async () => {
-  //   const data = await getDocs(collectionRef)
-  //   const dataFiltrada = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
-  //   console.table(dataFiltrada)
-  // }
-
-  // useEffect(() => {
-  //   getCollection();
-  // }, []);
-  ///FIREBASE - LLAMAR A TODOS LOS DOCUMENTOS
-
-  ///FIREBASE - LLAMAR A COLECION FILTRADA
-  // const productsRefFilter = query(
-  //   collection(db, "products"),
-  //   where("categoryId", "==", "discos"),
-  //   where("stock", "<", 10),
-  //   limit(1) //limita la respuesta
-  // )
-  ///FIREBASE - LLAMAR A COLECION FILTRADA
-
   return (
     <div className="App">
       <CartProvider>
--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -1,12 +1,104 @@
 import styles from './styles.module.scss';
+import { useContext, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { CartContext } from '../../context/CartContext';
+import confetti from 'canvas-confetti';
+import { collection, addDoc, doc, getDoc, updateDoc } from 'firebase/firestore';
+import { db } from "../../firebase/client";
 
 const Checkout = () => {
 
+    const [pedidoId, setPedidoId] = useState("");
+    const [cargando, setCargando] = useState(false);
+    const { carrito, precioTotal, vaciarCarrito } = useContext(CartContext);
+
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    
+    const comprar = async (data) => {
+        setCargando(true);
+        const order = {
+            cliente: data,
+            productos: carrito,
+            total: precioTotal()
+        }
+        vaciarCarrito()
+        console.log(order)
+
+        const orderRef = collection(db, "orders");
+        const orderDoc = await addDoc(orderRef, order);
+
+        for (const producto of carrito) {
+            const { id, cantidad } = producto;
+
+            const productRef = doc(db, "discos", id);
+            const productDoc = await getDoc(productRef);
+
+            if (productDoc.exists()) {
+                const stockActual = productDoc.data().stock;
+                const nuevoStock = stockActual - cantidad;
+
+                await updateDoc(productRef, { stock: nuevoStock });
+            }
+        }
+
+        setPedidoId(orderDoc.id);
+        setCargando(false);
+        confetti();
+
+    } 
+    
+    if (pedidoId) {
+        return (
+            <section className={styles['checkout__respuesta']}>
+                <h1>(づ ◕‿◕ )づ</h1>
+                <h1>🎉 Gracias por tu Compra 🎉 </h1>
+                <div className={styles['checkout__respuesta--id']}>
+                    <h2>Tu número de pedido es:</h2>
+                    <h3>{pedidoId}</h3>
+                </div>
+            </section>
+        )
+    }
+
     return (
         <section className={styles['checkout']}>
-            <h1>
-                CHEKOUT
-            </h1>
+            <h1>Finalizá tu Pedido</h1>
+            <form onSubmit={handleSubmit(comprar)}>
+                
+                <input type="text" placeholder="Ingresa tu Nombre" {...register("nombre", { required: true })} />
+                {errors.nombre &&
+                    <i>(¬､¬) ¡El Nombre es requerido!</i>
+                }
+
+                <input type="text" placeholder="Ingresá tu Email" {...register("email", { required: true })} />
+                {errors.email &&
+                    <i>(¬､¬) ¡El e-mail es requerido!</i>
+                }
+                
+                <input type="number" placeholder="Ingresá tu Teléfono"
+                    {...register("telefono", {
+                        required: true
+                    })} />
+                    
+                    {errors.telefono &&
+                        <i>(¬､¬) ¡El Teléfono es requerido!</i>
+                    }
+                
+                {cargando ? (
+                    <div className={styles['checkout__cargador']}>
+                        <h2>Enviando Pedido</h2>
+                        <span class="loader"></span>
+                    </div>
+                ): (
+                    <div className={styles['checkout__total']}>
+                        <h2>Total de tu compra: ${ precioTotal() }</h2>
+                        <button type="submit">CONFIRMAR COMPRA</button>
+                    </div>
+                    )
+                } 
+                
+
+            </form>
         </section>
     )
 }
